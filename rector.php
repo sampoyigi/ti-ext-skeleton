bf4ca217a8d099ee570076129e2b7f20a12b7a5d
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2025-05-08 23:59:12 +0100

    feat: add new Rector rules and update skipped rules in rector.php
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- rector.php
+++ rector.php
@@ -2,7 +2,10 @@
 
 declare(strict_types=1);
 
+use Rector\CodingStyle\Rector\Catch_\CatchExceptionNameMatchingTypeRector;
+use Rector\CodingStyle\Rector\ClassMethod\NewlineBeforeNewAssignSetRector;
 use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\ClassMethod\ReturnNeverTypeRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictNewArrayRector;
 use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
 
@@ -16,7 +19,10 @@ return RectorConfig::configure()
         DeclareStrictTypesRector::class,
     ])
     ->withSkip([
+        CatchExceptionNameMatchingTypeRector::class,
+        ReturnNeverTypeRector::class,
         ReturnTypeFromStrictNewArrayRector::class,
+        NewlineBeforeNewAssignSetRector::class,
     ])
     ->withPhpSets(php83: true)
     ->withPreparedSets(
